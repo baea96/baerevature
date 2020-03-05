f95a1db3399c638690017b9cb01d6f42f0728fcc
Alexander Bae <alexbaelafayette@gmail.com>
2020-03-05 13:38:32 -0500

    committed

---
+++ BaeRevature/src/main/java/com/revature/test/MyTest.java
@@ -0,0 +1,13 @@
+package com.revature.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyTest {
+	@Test
+	public void test() {
+		assertEquals("True is True.", true, true);
+	}
+
+}
